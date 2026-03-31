86661a7e02dac904efe26c09d52e1927171fc8b5
Junior <junior.diaz@cuidum.com>
2026-03-31 12:15:45 +0200

    Nextjs3

--- next-js-16-strapi-app/backend/types/generated/components.d.ts
+++ next-js-16-strapi-app/backend/types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ComponentLink extends Struct.ComponentSchema {
+  collectionName: 'components_component_links';
+  info: {
+    displayName: 'Link';
+    icon: 'apps';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'#'>;
+    isExternal: Schema.Attribute.Boolean;
+    label: Schema.Attribute.String;
+  };
+}
+
 export interface ComponentsLink extends Struct.ComponentSchema {
   collectionName: 'components_components_links';
   info: {
@@ -25,14 +40,15 @@ export interface LayoutHeroSection extends Struct.ComponentSchema {
     heading: Schema.Attribute.String;
     image: Schema.Attribute.Media<'images' | 'files'> &
       Schema.Attribute.Required;
-    link: Schema.Attribute.Component<'components.link', false>;
-    subHeading: Schema.Attribute.String;
+    link: Schema.Attribute.Component<'component.link', false>;
+    subHeading: Schema.Attribute.Text;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'component.link': ComponentLink;
       'components.link': ComponentsLink;
       'layout.hero-section': LayoutHeroSection;
     }
